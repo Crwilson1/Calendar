e8232aabf5d684503bc9fb0b51aed1a4ce9f857d
Crwilson1 <Crwilson1@hotmail.com>
2023-11-30 15:59:01 -0600

    Initial commit

---
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.calendar
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.Scope
+import com.google.api.services.calendar.CalendarScopes
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Set up Google Sign-In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestScopes(Scope(CalendarScopes.CALENDAR))
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // Sign in with Google
+        // Handle authentication and fetch calendar events here
+    }
+}
